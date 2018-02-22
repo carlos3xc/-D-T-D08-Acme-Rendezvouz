87f3df18d251824ceb98b34810a5b5761d4e5b5c
Miguel Bermudo <migberbay@gmail.com>
2018-02-22 18:38:54 +0100

    añadidos 2 controllers chanetes de rendezvous

---
+++ Acme-Rendezvous/src/main/java/controllers/user/RendezvousUserController.java
@@ -0,0 +1,155 @@
+package controllers.user;
+
+import java.util.Collection;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import security.LoginService;
+import security.UserAccount;
+import security.UserAccountService;
+import services.ActorService;
+import services.RendezvousService;
+import services.UserService;
+import controllers.AbstractController;
+import domain.Actor;
+import domain.Rendezvous;
+import domain.User;
+
+
+@Controller
+@RequestMapping("/rendezvous/user")
+public class RendezvousUserController extends AbstractController {
+
+	@Autowired
+	private RendezvousService rendezvousService;
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private UserAccountService userAccountService;
+	
+	@Autowired
+	private ActorService actorService;
+
+	public RendezvousUserController() {
+		super();
+	}
+
+	// Listing ----------------------------------------------------------------
+
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public ModelAndView list() {
+		ModelAndView result;
+
+		Collection<Rendezvous> rendezvouses;
+		rendezvouses = rendezvousService.findAll();
+
+		result = new ModelAndView("user/list");
+		result.addObject("rendezvouses", rendezvouses);
+		result.addObject("requestURI", "rendezvous/user/list.do");
+		return result;
+	}
+	
+//	// Profile ---------------------------------------------------------------
+//	@RequestMapping(value = "/profile", method = RequestMethod.GET)
+//	public ModelAndView list(@RequestParam final int userId) {
+//		ModelAndView result;
+//
+//		User user;
+//		user = userService.findOne(userId);
+//
+//		result = new ModelAndView("user/profile");
+//		result.addObject("user", user);
+//		result.addObject("requestURI", "user/profile.do");
+//		return result;
+//	}
+
+	// Creation ---------------------------------------------------------------
+
+	@RequestMapping(value = "/create", method = RequestMethod.GET)
+	public ModelAndView create() {
+		ModelAndView result;
+		Rendezvous rendezvous = this.rendezvousService.create();
+		//Se asume que el actor va a atender a su propio rendezvous asi que se le añade a la lista de atendidos
+		rendezvous.getListAttendants().add(e);
+		
+
+		result = this.createEditModelAndView(rendezvous);
+		return result;
+	}
+
+	// Edition ----------------------------------------------------------------
+
+	@RequestMapping(value = "/edit", method = RequestMethod.GET)
+	public ModelAndView edit(@RequestParam final int rendezvousId) {
+		ModelAndView result;
+		Rendezvous rendezvous = this.rendezvousService.findOne(rendezvousId);
+
+		Assert.notNull(rendezvous);
+		//TODO: Falta por comprobar que el usuario que esta editando los datos es el mismo que el que esta logueado.
+		Actor loggedActor = actorService.findByPrincipal();
+		User beingModified = rendezvousService.findOne(rendezvousId).getUser();
+		
+		Assert.isTrue(loggedActor.equals(beingModified));
+		
+		result = this.createEditModelAndView(rendezvous);
+		return result;
+	}
+
+	@RequestMapping(value = "/edit", method = RequestMethod.POST, params = "save")
+	public ModelAndView save(@Valid final Rendezvous rendezvous, final BindingResult binding) {
+		ModelAndView result;
+
+		if (binding.hasErrors())
+			result = this.createEditModelAndView(rendezvous);
+		else
+			try {
+				this.rendezvousService.save(rendezvous);
+				result = new ModelAndView("redirect:list.do");
+			} catch (final Throwable oops) {
+				result = this.createEditModelAndView(rendezvous, "rendezvous.commit.error");
+			}
+		return result;
+	}
+
+	@RequestMapping(value = "/edit", method = RequestMethod.POST, params = "delete")
+	public ModelAndView delete(final User user, final BindingResult binding) {
+		ModelAndView result;
+
+		try {
+			this.userService.delete(rendezvous);
+			result = new ModelAndView("redirect:list.do");
+		} catch (final Throwable oops) {
+			result = this.createEditModelAndView(rendezvous, "user.commit.error");
+		}
+		return result;
+	}
+
+	// Ancillary methods ------------------------------------------------------
+
+	protected ModelAndView createEditModelAndView(final Rendezvous rendezvous) {
+		ModelAndView result;
+
+		result = this.createEditModelAndView(rendezvous, null);
+		return result;
+	}
+
+	protected ModelAndView createEditModelAndView(final Rendezvous rendezvous,
+			final String message) {
+		ModelAndView result;
+		result = new ModelAndView("rendezvous/edit");
+		result.addObject("rendezvous", rendezvous);
+		result.addObject("message", message);
+		return result;
+	}
+}
